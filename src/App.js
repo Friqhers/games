d826793a68a43d8ca8c82cf8c1d8290f6e2f346b
Mehmet Furkan Gülmez <32138375+Friqhers@users.noreply.github.com>
2019-07-03 16:42:59 +0300

    App and Index changed
    queries.listGames

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { API, graphqlOperation } from 'aws-amplify'
+import Query from "../src/graphql/queries"
+
+
+
+class App extends Component {
+  state = { games: [] }
+  async componentDidMount() {
+    const data = await API.graphql(graphqlOperation(Query.listGames))
+    this.setState({
+      games: data.data.listGames.items
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+          {this.state.games.map((game,index)=>{
+            <p key={index}>
+              {game.name}
+            </p>
+
+          })}
+      </div>
+    );
+  }
 }
 
 export default App;
